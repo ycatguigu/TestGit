0b478becedecc20f4f51671877d762a1d8df141c
zhangpanpan <zhppyc@163.com>
2019-06-17 15:08:57 +0800

    edit on branch hot_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,7 +3,8 @@ package com.atguigu.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-		System.out.println("right.....");
+		System.out.println("right1.....");
+		System.out.println("hot_fix...");
 	}
 
 }
